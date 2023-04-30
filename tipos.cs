7a470c09232ba9d3ce84df05729ff590b7453593
thamirespereira <thamiresfrpereira@hotmail.com>
2023-04-30 18:58:43 -0300

    tipos de dados

---
+++ tipos.cs
@@ -0,0 +1,39 @@
+public class Tipos{
+
+    //int
+    int i = 10;
+
+    //float
+    float 20.9f;
+
+    //double
+    double d = 10.9;
+
+    //decimal
+    decimal c =30.9m;
+
+    //bool
+    bool b1 = true;
+    bool b2 = false;
+
+    //char
+    char c2 = 'A';
+
+    //string
+    string s = "Thamires";
+
+    //var
+    var cidade = "Nova Iguaçu";
+    var idade = 26;
+
+    idade = 27;
+
+    //const
+    const string pais = "Brasil";
+
+    pais = "Australia"; //erro
+
+    //conversao de tipos
+    f = idade; // 
+    f = (float) c; //decimal para float
+}
